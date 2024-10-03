cac80f13fdcbf0d7ed31d3488ae393bd199c7535
Gear <84141000+macgeargear@users.noreply.github.com>
2024-10-04 01:39:34 +0700

    feat: core theme utils

---
+++ lib/core/theme/app_color.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: use_full_hex_values_for_flutter_colors
+
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color background = Colors.white;
+  static const Color white = Colors.white;
+  static const Color lightGrey = Color(0xd5d5d5d5);
+  static const Color grey = Color(0xd5d5d5d5);
+  static const Color darkGrey = Color(0x003F3A3A);
+  static const Color error = Colors.redAccent;
+  static const Color transparent = Colors.transparent;
+
+  static const Color purple = Color(0xFF86469C);
+
+  static const Color primary = Color(0xFFFCA4C4);
+  static const Color secondary = Color(0xFFFCC7DC);
+  static const Color accent = Color(0xFFF6F4D4);
+}
---
+++ lib/core/theme/theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:t2dm_mobile/core/theme/app_color.dart';
+
+class AppTheme {
+  static _border([Color color = AppColor.lightGrey]) => OutlineInputBorder(
+      borderSide: BorderSide(color: color, width: 2),
+      borderRadius: BorderRadius.circular(10));
+
+  static final lightMode = ThemeData.light().copyWith(
+    scaffoldBackgroundColor: AppColor.background,
+    inputDecorationTheme: InputDecorationTheme(
+      enabledBorder: _border(),
+      focusedBorder: _border(AppColor.secondary),
+    ),
+    appBarTheme: const AppBarTheme(backgroundColor: AppColor.background),
+  );
+}
