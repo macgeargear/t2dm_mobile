eefc53e1f14d22c34d91f85b67a45dab5a73d37a
Gear <84141000+macgeargear@users.noreply.github.com>
2024-10-04 01:39:51 +0700

    feat: auth page

---
+++ lib/features/auth/presentation/pages/signin_page.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:t2dm_mobile/core/common/widgets/loader.dart';
+import 'package:t2dm_mobile/core/theme/app_color.dart';
+import 'package:t2dm_mobile/core/utils/show_snackbar.dart';
+import 'package:t2dm_mobile/features/auth/presentation/auth/auth_bloc.dart';
+import 'package:t2dm_mobile/features/auth/presentation/pages/signup_page.dart';
+import 'package:t2dm_mobile/features/auth/presentation/widgets/auth_button.dart';
+import 'package:t2dm_mobile/features/auth/presentation/widgets/auth_field.dart';
+
+class SignInPage extends StatefulWidget {
+  static route() => MaterialPageRoute(builder: (context) => const SignInPage());
+  const SignInPage({super.key});
+
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: BlocConsumer<AuthBloc, AuthState>(
+          listener: (context, state) {
+            if (state is AuthFailure) {
+              showSnackBar(context, state.message);
+            }
+          },
+          builder: (context, state) {
+            if (state is AuthLoading) {
+              return const Loader();
+            }
+            return Form(
+              key: formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Sign In',
+                    style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 30),
+                  AuthField(hintText: 'Email', controller: emailController),
+                  const SizedBox(height: 15),
+                  AuthField(
+                    hintText: 'Password',
+                    controller: passwordController,
+                    isObscureText: true,
+                  ),
+                  const SizedBox(height: 20),
+                  AuthButton(
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {}
+                    },
+                    buttonText: 'Sign In',
+                  ),
+                  const SizedBox(height: 20),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(context, SignUpPage.route());
+                    },
+                    child: RichText(
+                        text: TextSpan(
+                            text: 'Don\'t have an account? ',
+                            style: Theme.of(context).textTheme.titleMedium,
+                            children: [
+                          TextSpan(
+                              text: 'Sign Up',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleMedium
+                                  ?.copyWith(
+                                      color: AppColor.primary,
+                                      fontWeight: FontWeight.bold))
+                        ])),
+                  )
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:t2dm_mobile/core/common/widgets/loader.dart';
+import 'package:t2dm_mobile/core/theme/app_color.dart';
+import 'package:t2dm_mobile/core/utils/show_snackbar.dart';
+import 'package:t2dm_mobile/features/auth/presentation/auth/auth_bloc.dart';
+import 'package:t2dm_mobile/features/auth/presentation/pages/signin_page.dart';
+import 'package:t2dm_mobile/features/auth/presentation/widgets/auth_button.dart';
+import 'package:t2dm_mobile/features/auth/presentation/widgets/auth_field.dart';
+
+class SignUpPage extends StatefulWidget {
+  static route() => MaterialPageRoute(builder: (context) => const SignUpPage());
+
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final passwordController = TextEditingController();
+  final usernameController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    passwordController.dispose();
+    usernameController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: BlocConsumer<AuthBloc, AuthState>(
+          listener: (context, state) {
+            if (state is AuthFailure) {
+              showSnackBar(context, state.message);
+            }
+          },
+          builder: (context, state) {
+            if (state is AuthLoading) {
+              return const Loader();
+            }
+            return Form(
+                key: formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        const Text('Sign Up',
+                            style: TextStyle(
+                                fontSize: 50,
+                                fontWeight: FontWeight.bold,
+                                color: AppColor.primary)),
+                        const Text('the best health care platform',
+                            style: TextStyle(
+                                fontSize: 20, color: AppColor.primary)),
+                        const SizedBox(height: 300),
+                        AuthField(
+                            hintText: 'Username',
+                            controller: usernameController),
+                        const SizedBox(height: 15),
+                        AuthField(
+                          hintText: 'Password',
+                          controller: passwordController,
+                          isObscureText: true,
+                        ),
+                        const SizedBox(height: 20),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+                      child: Column(
+                        children: [
+                          AuthButton(
+                            onPressed: () {
+                              if (formKey.currentState!.validate()) {}
+                            },
+                            buttonText: 'Sign Up',
+                          ),
+                          const SizedBox(height: 20),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(context, SignInPage.route());
+                            },
+                            child: RichText(
+                                text: TextSpan(
+                                    text: 'Already have an account? ',
+                                    style:
+                                        Theme.of(context).textTheme.titleMedium,
+                                    children: [
+                                  TextSpan(
+                                      text: 'Sign In',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .titleMedium
+                                          ?.copyWith(
+                                              color: AppColor.primary,
+                                              fontWeight: FontWeight.bold))
+                                ])),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ));
+          },
+        ),
+      ),
+    );
+  }
+}
