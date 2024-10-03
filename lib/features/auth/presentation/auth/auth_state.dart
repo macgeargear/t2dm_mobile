8088aafd2ec5289eeb6f335e25376d60ae977815
Gear <84141000+macgeargear@users.noreply.github.com>
2024-10-04 01:39:19 +0700

    feat: auth bloc

---
+++ lib/features/auth/presentation/auth/auth_state.dart
@@ -0,0 +1,19 @@
+part of 'auth_bloc.dart';
+
+sealed class AuthState {
+  const AuthState();
+}
+
+final class AuthInitial extends AuthState {}
+
+final class AuthLoading extends AuthState {}
+
+final class AuthSuccess extends AuthState {
+  final String user;
+  const AuthSuccess(this.user);
+}
+
+final class AuthFailure extends AuthState {
+  final String message;
+  const AuthFailure(this.message);
+}
